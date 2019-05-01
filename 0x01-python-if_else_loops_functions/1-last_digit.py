e3c3c1d43352a3c1efed86d6b464e953d37e0acb
Cyn <cmdominguez@gmail.com>
2019-05-01 00:17:46 +0000

    Shorten intro as variable for pep8 compliance

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
+intro = "Last digit of "
 # Output to print Last digit of _number is _truncated number_
 # and is EITHER greater than 5, 0, or less than 6 and not 0
 if last > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last))
+    print("{}{} is {} and is greater than 5".format(intro, number, last))
 elif last == 0:
-    print("Last digit of {} is {} and is 0".format(number, last))
+    print("{}{} is {} and is 0".format(intro, number, last))
 elif last < 6:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last))
+    print("{}{} is {} and is less than 6 and not 0".format(intro, number, last))
