e14c5ded33be39aa832cefa78fbd0889e9c12f08
Cyn <cmdominguez@gmail.com>
2019-05-01 01:10:22 +0000

    shorten string for pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,6 +5,7 @@ last = abs(number) % 10
 if number < 0:
     last = -(last)
 intro = "Last digit of "
+six = " and is less than 6 and not 0"
 # Output to print Last digit of _number is _truncated number_
 # and is EITHER greater than 5, 0, or less than 6 and not 0
 if last > 5:
@@ -12,4 +13,4 @@ if last > 5:
 elif last == 0:
     print("{}{} is {} and is 0".format(intro, number, last))
 elif last < 6:
-    print("{}{} is {} and is less than 6 and not 0".format(intro, number, last))
+    print("{}{} is {}{}".format(intro, number, last, six))
