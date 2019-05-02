3363ff9e1efbd9b084b9ec657e8963bb043f13c7
Cyn <cmdominguez@gmail.com>
2019-05-02 23:36:30 +0000

    adding back comments without checker word 1-calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Note: the filename passed into from <filename> import <function names>
+contains functions for addition, subraction,
+multiplication, and division.
+"""
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
@@ -7,3 +12,11 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
+"""
+If the module is imported, the code is not run, so "import add, sub, mul,
+or div" will not run code. This is often used either to provide a convenient
+user interface to a module, or for testing purposes like running the module
+as a script executes a test suite.
+https://docs.python.org/3.4/tutorial/modules.html
+"""
+""""""""
