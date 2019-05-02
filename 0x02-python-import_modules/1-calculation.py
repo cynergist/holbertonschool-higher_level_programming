55696db23d9a988a9faae5fce45e8014f692aff1
Cyn <cmdominguez@gmail.com>
2019-05-02 22:32:49 +0000

    remove some comments

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 """
-First make the file usable as a script as well as an importable module
-because the code that parses the command line only runs if the module is
-executed as the "main" file
-
 Note: the file calculator_1 contains functions for addition, subraction,
 multiplication, and division. [from <filename> import <function name(s)]
 """
@@ -17,7 +13,7 @@ if __name__ == "__main__":
     print("{} / {} = {}".format(a, b, div(a, b)))
 """
 If the module is imported, the code is not run, so "import add, sub, mul,
-or div" will notrun code. This is often used either to provide a convenient
+or div" will not run code. This is often used either to provide a convenient
 user interfaceto a module, or for testing purposes like running the module
 as a scriptexecutes a test suite.
 https://docs.python.org/3.4/tutorial/modules.html
