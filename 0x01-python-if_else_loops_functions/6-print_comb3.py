2ca88f91d2aa0fb033770d3e3b34374b1142400b
Cyn <cmdominguez@gmail.com>
2019-05-02 07:06:10 +0000

    refactored for portability

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,7 @@
 for i in range(0, 10):
         for j in range((i + 1), 10):
                 if i is 8 and j is 9:
-                        print("{:d}{:d}, ".format(i, j), end="")
+                        print("{:d}{:d}".format(i, j), end="")
                 else:
                         print("{:d}{:d}, ".format(i, j), end="")
+print()
