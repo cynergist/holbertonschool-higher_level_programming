eb6868de6f9c96cb2bebe2bee3813f6abe543e98
Cyn <cmdominguez@gmail.com>
2019-05-03 00:50:56 +0000

    remove comment

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,8 +2,3 @@
 if __name__ == "__main__":
     from variable_load_5 import a
     print("{}".format(a))
-
-"""
-Note: the filename passed into from <filename>
-import <function name> imports a simple variable
-"""
