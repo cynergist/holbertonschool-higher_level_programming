e2d4720b07f5cca4b7df5e5aab509ae81d7641fb
Cyn <cmdominguez@gmail.com>
2019-05-03 00:40:22 +0000

    add 5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print("{}".format(a))
+
+"""
+Note: the filename passed into from <filename>
+import <function name> imports a simple variable
+"""
