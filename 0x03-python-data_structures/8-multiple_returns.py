0217347974fbfebeee6b921bf8c7f4a60a2ce38b
Cyn <cmdominguez@gmail.com>
2019-05-08 06:47:54 +0000

    fix return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,4 @@
 def multiple_returns(sentence):
     if sentence:
         return len(sentence), sentence[0]
-    else:
-        sentence[0] = None
-        return len(sentence), sentence[0]
+    return len(sentence), None
