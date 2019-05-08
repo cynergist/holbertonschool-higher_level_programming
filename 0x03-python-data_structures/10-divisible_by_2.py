c05921e38d46a1c865b8e6ab07d9250fc5cd1f12
Cyn <cmdominguez@gmail.com>
2019-05-08 23:17:49 +0000

    add 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    # Return a new list with True or False
+    return[True if x % 2 == 0 else False for x in my_list]
+'''
+This is a list comprehension after the return statement using
+ternary operators and the list comprehension syntax
+'''
