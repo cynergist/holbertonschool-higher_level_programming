91234237ea3438e410956df6ff8dc2644f301018
Cyn <cmdominguez@gmail.com>
2019-05-09 02:58:04 +0000

    correcting typo in del statement

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     '''
-    function that deletes the item at a specific position in a list
+    Function that deletes the item at a specific position in a list
     if idx is negative or out of range, nothing changes
     '''
     if idx >= 0 or idx < len(my_list):
