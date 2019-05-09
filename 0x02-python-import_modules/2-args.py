ca0b538f26ca385b18e15f40dbca100b1acc8dc9
Cyn <cmdominguez@gmail.com>
2019-05-09 08:08:22 +0000

    add 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,17 @@
 if __name__ == "__main__":
     """
     Note: it is possible to use sys, but we are only concerned
-    withe argv, so will only import this module
+    with the argv, so will only import this module
     """
     from sys import argv
 
-    if len(argv) is 2:
-        print("1 argument:\n1: {}".format(str(argv[1]))
-    elif len(argv) > 2:
-        print("{} arguments:".format(len(argv) - 1))
-        for i in range(len(argv) - 1):
-              print("{:d}: {:s}\n".format(i + 1, argv[i + 1]))
-    else:
+    len = len(argv)
+
+    if len is 1:
         print("0 arguments.")
+    elif len is 2:
+        print("1 argument:\n1: {:s}".format(argv[1]))
+    else:
+        print("{:d} arguments:".format(len - 1))
+        for args in range(1, len):
+            print("{:d}: {:s}".format(args, argv[args]))
