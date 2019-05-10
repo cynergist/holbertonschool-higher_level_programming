959da6fcdb83648b76fd0ee6bf67d8e6b6a063ca
Cyn <cmdominguez@gmail.com>
2019-05-10 06:34:22 +0000

    remove comments

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    """ Use lambda in convention "lambda key: key operator number, listname"
-    """
     return list(map(lambda x: x * number, my_list))
