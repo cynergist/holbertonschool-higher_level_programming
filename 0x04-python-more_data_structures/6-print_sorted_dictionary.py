d11424d492bc4846cf455a2115108f1c2e3b1806
Cyn <cmdominguez@gmail.com>
2019-05-10 03:01:24 +0000

    add 6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+# store your key in a variable and your value in a variable
+    for key, value in sorted(a_dictionary.items()):
+#        sorted(key)
+        print("{}: {}".format(key, value))
+# sorted(a_dictionary.key=str.lower)
