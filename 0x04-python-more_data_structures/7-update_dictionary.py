0ece6916fe8a949d893ff56ba10e71b71b152777
Cyn <cmdominguez@gmail.com>
2019-05-10 04:11:08 +0000

    add 7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """
+    function replaces or adds key/value in a dictionary
+    take the key of our dictionary and pair with the value
+    in our argument
+    """
+    a_dictionary[key] = value
+    # return our dictionary
+    return a_dictionary
