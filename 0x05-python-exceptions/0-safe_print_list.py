1c1c6f99cfe0f3fa3539687ef4554225b29e34a7
Cyn <cmdominguez@gmail.com>
2019-05-21 04:58:28 +0000

    remove pep8 non-compliant comment

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,5 @@
 #/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-# function prints x elements of a list without importing a module or using len()
     count = 0
     try:
         for i in range(x):
