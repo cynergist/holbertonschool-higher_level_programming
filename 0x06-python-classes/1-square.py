8c22e7e739680178684ded71eafd5b6d5687c1a2
Cyn <cmdominguez@gmail.com>
2019-05-22 00:06:47 +0000

    add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A class for a Square """
+
+
+class Square:
+
+    def __init__(self, size):
+        """Args: param1 (__size): Size of the square, a private attribute
+        """
+        self.__size = size
