a1de75a9e1a92631c0fb30df26948407b5b31961
Cyn <cmdominguez@gmail.com>
2019-06-06 05:02:36 +0000

    pep8 fixes

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ Function that checks if obj isinstance of a_class """
 
+
 def is_kind_of_class(obj, a_class):
-    """ Args: param1 (obj):
-    param2 (a_class):
+    """ Args: param1 (obj): instance
+    param2 (a_class): type of object to check
     """
     if isinstance(obj, a_class):
         return True
