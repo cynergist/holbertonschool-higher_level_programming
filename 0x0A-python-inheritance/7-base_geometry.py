a8ae071cb57cb7946b3964993af069477cc56ef7
Cyn <cmdominguez@gmail.com>
2019-06-08 22:20:16 +0000

    update dynamic name in exception

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         if type(value) is not int:
-            raise TypeError("name must be an integer")
+            raise TypeError(name + " must be an integer")
         if value <= 0:
-            raise ValueError("age must be greater than 0")
-            raise ValueError("distance must be greater than 0")
+            raise ValueError(name + " must be greater than 0")
+            raise ValueError(name + " must be greater than 0")
