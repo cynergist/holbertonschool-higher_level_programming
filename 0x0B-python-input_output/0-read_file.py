5a5afd30f5a0c5b21b6f9ddf1f1878b1e2d246b2
Cyn <cmdominguez@gmail.com>
2019-06-08 00:39:49 +0000

    updated for .read() to print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os
+
 
 """ a function that reads a text file (UTF8) and prints it to stdout
 """
@@ -7,15 +7,14 @@ import os
 
 def read_file(filename=""):
 
-    with open(filename, mode="r", encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read(), end="")
 
-            """ Arg1: string containing filename
-            Arg2: mode can be 'r' when file will only be read,
-            'w' for writing, and 'a' opens file for appending, 'r+
-            for both reading and writing
-            , overwrite mode, unicode where nos represent
-            characters
-            Arg3: UTF-8 encoding
-            """
+        """ Arg1: string containing filename
+        Arg2: mode can be 'r' when file will only be read,
+        'w' for writing, and 'a' opens file for appending, 'r+
+         for both reading and writing
+         , overwrite mode, unicode where nos represent
+         characters
+         Arg3: UTF-8 encoding
+         """
