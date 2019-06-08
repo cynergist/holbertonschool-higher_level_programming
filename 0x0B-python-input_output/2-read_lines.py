31de24e5990b9dde5773f7fc34e8c8940c590233
Cyn <cmdominguez@gmail.com>
2019-06-08 02:34:40 +0000

    add documentation and fix validation stmts

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 
 
-def read_lines(filename="", nb_lines=0):
+"""
+a function that reads n lines of a UTF8 text file and prints to stdout
+"""
 
+
+def read_lines(filename="", nb_lines=0):
+    """
+    Arg1: filename: myfile
+    Arg2: nb_lines: number of lines in the file
+    """
     with open(filename, mode="r", encoding="utf-8") as myfile:
-#        count = 1
-#        if len(myfile.readlines()) < nb_lines <= 0:
-        if len(myfile.readlines()) < nb_lines:
+        if nb_lines <= 0:
             print(myfile.read(), end="")
         else:
             for count in range(nb_lines):
                 print(myfile.readline(), end="")
-                print(count)
-                print(len(myfile.readlines()))
