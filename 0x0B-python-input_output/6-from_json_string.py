4eec058cffdabc1eb33ce672cdac36454f7f7967
Cyn <cmdominguez@gmail.com>
2019-06-09 07:06:19 +0000

    add 6-from_json_string.py

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+
+'''
+a function that returns a Python data struct object represented by a JSON string
+'''
+
+
+def from_json_string(my_str):
+    '''
+    Arg1: a string object
+    '''
+    return json.JSONDecoder().decode(my_str)
