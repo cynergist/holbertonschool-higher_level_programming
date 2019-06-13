8bceb6b5e155afb4162d3d4a78053d12027bfa40
Cyn <cmdominguez@gmail.com>
2019-06-13 01:39:49 +0000

    convert to setUpClass(cls)

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+xc#!/usr/bin/python3
 ''' Unittest for base.py '''
 
 from models.base import Base
@@ -8,60 +8,38 @@ import unittest
 
 class TestBase(unittest.TestCase):
     ''' Unittest for base '''
+
+    @classmethod
+    def setUpClass(cls):
+        ''' Setting up testing environment for Base '''
+
+        Base._Base__nb_objects = 0
+        cls.b1 = Base()
+        cls.b10 = Base()
+        cls.b2 = Base(0)
+        cls.b3 = Base(50)
+        cls.b4 = Base(-5)
+        cls.b5 = Base(256)
+
     def test_class_type(self):
         ''' Unittest for correct class type '''
-        b = Base()
-        self.assertTrue(type(b), Base)
+        self.assertEqual(type(self.b1), Base)
 
     def test_instantiation(self):
         ''' Unittest for instantiation of class '''
-        b = Base()
-        self.assertTrue(b, Base)
-    '''
-    def test_no_arg(self):
-        Unittest for base incrementation beginning with 1 for id
-        b = Base()
-        self.assertEqual(b.id, 1)
-    '''
-    def test_no_list(self):
-        ''' Unittest for empty list '''
-        b = Base([])
-        self.assertEqual(b.id, [])
+        self.assertTrue(self.b1)
 
-    def test_no_tuple(self):
-        ''' Unittest for empty tuple '''
-        b = Base(())
-        self.assertEqual(b.id, ())
+    def test_incrementation(self):
+        ''' Unittest for incrementation of __nb_objects '''
+        self.assertEqual(self.b1.id, 1)
+        self.assertEqual(self.b10.id, 2)
 
-    def test_no_dict(self):
-        ''' Unittest for empty dictionary '''
-        b = Base({})
-        self.assertEqual(b.id, {})
-
-    def test_one_arg(self):
-        ''' Unittest for base using 10 for id'''
-        b = Base(10)
-        self.assertEqual(b.id, 10)
-
-    def test_negative_value(self):
-        ''' Unittest for base using -1 for id '''
-        b = Base(-1)
-        self.assertEqual(b.id, -1)
-
-    def test_zero_value(self):
-        ''' Unittest for base using 0 for id '''
-        b = Base(0)
-        self.assertEqual(b.id, 0)
-
-    def test_string(self):
-        ''' Unittest for base using "hello" for id '''
-        b = Base('hello')
-        self.assertEqual(b.id, 'hello')
-
-    def test_dict(self):
-        ''' Unittest for base using a dict for id '''
-        b = Base({'name': 'cyn', 'address': 'berkeley'})
-        self.assertEqual(b.id, {'name': 'cyn', 'address': 'berkeley'})
+    def test_ints(self):
+        ''' Unittest id passing 0,pos,neg,outside nsmallposints '''
+        self.assertEqual(self.b2.id, 0)
+        self.assertEqual(self.b3.id, 50)
+        self.assertEqual(self.b4.id, -5)
+        self.assertEqual(self.b5.id, 256)
 
 if __name__ == '__main__':
     unittest.main()
