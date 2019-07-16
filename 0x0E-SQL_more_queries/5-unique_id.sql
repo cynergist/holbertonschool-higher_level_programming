55d3c8d548736bbc9c889191403424e3d65bbe8b
Cyn <cmdominguez@gmail.com>
2019-07-16 22:27:24 +0000

    Truncate comment line

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- script creates the table unique_id on my MySQL server.
--- unique_id description: id INT, default value 1 and is unique, name VARCHAR(256)
+-- unique_id desc: id INT, default value 1 and is unique, name VARCHAR(256)
 -- If the table unique_id already exists, script does not fail
 CREATE TABLE IF NOT EXISTS `unique_id`
 (
