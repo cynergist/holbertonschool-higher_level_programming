a6b6431e50a806678815da0ad7cfca07bd13f94a
Cyn <cmdominguez@gmail.com>
2019-07-18 02:32:29 +0000

    add 13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+ -- script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT name as genre, COUNT(*) as number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres ON
+tv_show_genres.genre_id=tv_genres.id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
