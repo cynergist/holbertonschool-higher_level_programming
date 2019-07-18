e7d068c6952be834be629501fcdefb9ed0452b27
Cyn <cmdominguez@gmail.com>
2019-07-18 02:55:42 +0000

    Include table name in SELECT statement

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
- -- script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
-SELECT name as genre, COUNT(*) as number_of_shows
+ -- script that lists all shows contained in hbtn_0d_tvshows
+-- without a genre linked.
+SELECT tv_genres.name as genre, COUNT(*) as number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON
 tv_show_genres.genre_id=tv_genres.id
