166b86fa96eae7cdda54c1faa41308d42da45529
Cyn <cmdominguez@gmail.com>
2019-07-18 02:59:42 +0000

    Use INNER JOIN instead of LEFT JOIN

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- without a genre linked.
 SELECT tv_genres.name as genre, COUNT(*) as number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres ON
+INNER JOIN tv_show_genres ON
 tv_show_genres.genre_id=tv_genres.id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
