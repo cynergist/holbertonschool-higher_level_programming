6c133423b6acf0dfbd376065450267c610a829d2
Cyn <cmdominguez@gmail.com>
2019-07-18 03:06:30 +0000

    Use table name for GROUP statement

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
  -- script that lists all shows contained in hbtn_0d_tvshows
 -- without a genre linked.
-SELECT tv_genres.name as genre, COUNT(*) as number_of_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
-INNER JOIN tv_show_genres ON
-tv_show_genres.genre_id=tv_genres.id
-GROUP BY genre
+INNER JOIN tv_show_genres
+ON tv_show_genres.genre_id=tv_genres.id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
