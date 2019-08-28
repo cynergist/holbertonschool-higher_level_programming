341547345964c454aa9fcdccfdfe8bfb67c02042
Cyn <cmdominguez@gmail.com>
2019-08-28 22:43:02 +0000

    Update type and content output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ https://intranet.hbtn.io/status
 '''
 if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}".format(r.text))
-    print("\t- content: {}".format(r.headers['content-type']))
+    print("Body response:\n\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
