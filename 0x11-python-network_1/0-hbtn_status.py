601b052a4d7d406d0c544c4b5507a5ddb2f085f8
Cyn <cmdominguez@gmail.com>
2019-08-28 02:27:17 +0000

    decode utf8 for specific content vs entire body response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 import urllib.parse
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        data = response.read().decode('utf-8')
+        data = response.read()
     print("Body response:\n\t- type: {}".format(type(data)))
     print("\t- content: {}".format(data))
-    print("\t- utf8 content: {}".format('utf-8'))
+    print("\t- utf8 content: {}".format(data.decode('utf-8')))
