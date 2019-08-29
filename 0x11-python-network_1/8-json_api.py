b03a2847e47b8e5359eb219525ae8891c46ab218
Cyn <cmdominguez@gmail.com>
2019-08-29 21:10:13 +0000

    Add documentation and debug dictionary to json format conversion

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,24 +8,24 @@ from sys import argv
 
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
-    params = {'q': ''}
-    if len(argv) > 1:  # Get address from command line
-        params['q'] = argv[1]  # Add value at argv[1] to our 'q' key
-    r = requests.post(url, params=params)
+    # q parameter is our line argument
+    q = argv[1] if len(argv) > 1 else ''
+    # Make a post request with the parameter and convert to json
+    r = requests.post(url, data={'q': q}).json()
     '''
-    With POST, form data appears within the message body of the HTTP request
+    With POST, form data 'id' and 'name' appear within the message body
+    of the HTTP request upon converting to json
     '''
-    try:
-        body = r.json()
-        if body is False:
-            print('No result')
-        else:
-            print('[{}] {}'.format(body.get('id'), body.get('name')))
-    except Exception:
+    if 'id' in r or 'name' in r is not None:
+        print('[{}] {}'.format(r['id'], r['name']))
+    # Check that our response returned objects from the API
+    elif 'id' in r or 'name' in r is None:
         print('Not a valid JSON')
+    else:
+        print('No result')
 
 '''
-Example return:
+Example output:
 $ ./8-json_api.py
 No result
 $ ./8-json_api.py a
