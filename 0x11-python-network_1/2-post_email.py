4ec186c3ca400f447d0b2003dfea8a58d977a5dc
Cyn <cmdominguez@gmail.com>
2019-08-29 00:47:53 +0000

    add 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Script that takes in a URL and an email, sends a post request,
+to the passed URL w/ email as param, and displays the body of the response
+'''
+import urllib.request
+import urllib.parse
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = dict(email=argv[2])
+
+    data = urllib.parse.urlencode(values).encode('utf-8')
+    r = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(r) as response:
+        ''' The returned response object gives us access to all headers '''
+        url_page = response.read()
+        print(url_page.decode())
