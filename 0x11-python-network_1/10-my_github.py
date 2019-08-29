8f0410939d26202ecf97e8c796163e936393de27
Cyn <cmdominguez@gmail.com>
2019-08-29 23:20:39 +0000

    Account for wrong credentials used

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,18 +9,19 @@ if __name__ == '__main__':
     Per https://developer.github.com/v3/#basic-authentication
     all data is sent and received as JSON
     '''
-    url = 'https://api.github.com'
+    url = 'https://api.github.com/user'
     # Command line args are username and password, respectively
     r = requests.get(url, auth=(argv[1], argv[2]))
-
     jsonify = r.json()
-
-    print(jsonify.get('id'))
+    if 'id' in jsonify:
+        print(jsonify['id'])
+    else:
+        print(None)
 
 '''
 Example output:
-$ ./10-my_github.py papamuziko cisfun
+$ ./10-my_github.py cynergist cisfun
 2531536
-$ ./10-my_github.py papamuziko wrong_pwd
+$ ./10-my_github.py cynergist wrong_pwd
 None
 '''
