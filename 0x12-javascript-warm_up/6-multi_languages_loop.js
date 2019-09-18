11935eb2d771d6174dafbc283bfbdcf6be4cbcc2
Cyn <cmdominguez@gmail.com>
2019-09-18 06:32:08 +0000

    add 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const myArray = ['C is fun\nPython is cool\nJavascript is amazing'];
+// A way to print to output the statements using a for loop.
+for (let i = 0; i < myArray.length; i++) console.log(myArray[i]);
